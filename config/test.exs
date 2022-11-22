e9595a11115428dede123dc27db23cd69a7c4f17
Finn Behrens <me@kloenk.dev>
2022-11-22 14:31:06 +0100

    import CI from Clapper

--- config/test.exs
+++ config/test.exs
@@ -1,1 +1,12 @@
 import Config
+
+config :nostrum,
+  token: System.get_env("FLEET_BOT_TOKEN", "")
+
+config :fleet_bot, FleetBot.Repo,
+  username: "fleet_bot_test",
+  password: "fleet_bot_test",
+  database: "fleet_bot_test",
+  hostname: "localhost",
+  stacktrace: true,
+  show_sensitive_data_on_connection_error: true
--- mix.exs
+++ mix.exs
@@ -52,6 +52,7 @@ defmodule FleetBot.MixProject do
       setup: ["deps.get", "ecto.setup"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
+      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
       fmt: ["format"]
     ]
   end
